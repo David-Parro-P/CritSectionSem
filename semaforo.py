fc845c571d9a09c96ebe044ee2dc5ea1de8f3306
Pablo <pabsan16@ucm.es>
2023-02-16 13:43:55 +0100

    practica lock sem

---
+++ semaforo.py
@@ -0,0 +1,47 @@
+from multiprocessing import Process
+from multiprocessing import current_process
+from multiprocessing import Value, Array, BoundedSemaphore
+import multiprocessing
+import time
+import random
+N = 8
+
+def is_anybody_inside(critical, tid):
+    found = False
+    i = 0
+    while i<len(critical) and not found:
+        found = tid!=i and critical[i]==1
+        i += 1
+    return found
+def task(common, tid, sem):
+    a = 0
+    for i in range(100):
+        print(f'{tid}−{i}: Non−critical Section')
+        a += 1
+        print(f'{tid}−{i}: End of non−critical Section')
+        sem.acquire()
+        print(f'{tid}−{i}: Critical section')
+        v = common.value + 1
+        time.sleep(random.random())
+        print(f'{tid}−{i}: Inside critical section')
+        common.value = v
+        print(f'{tid}−{i}: End of critical section')   
+        sem.release()
+def main():
+    sem = BoundedSemaphore(1)
+    lp = []
+    common = Value('i', 0)
+    critical = Array('i', [0]*N)
+    turn = Value('i', 0)
+    for tid in range(N):
+        lp.append(Process(target=task, args=(common, tid,sem)))
+    print (f"Valor inicial del contador {common.value}")
+    for p in lp:
+        p.start()
+    for p in lp:
+        p.join()
+    print (f"Valor final del contador {common.value}")
+    print ("fin")
+if __name__ == "__main__":
+    main()
+
